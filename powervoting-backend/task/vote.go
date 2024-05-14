7f491a921be4e2f635e02d5098f9b20f376988e2
yu zhang <zhangyu@storswift.com>
2024-05-14 10:37:17 +0000

    feat: remove load bearing sleeps

--- powervoting-backend/task/vote.go
+++ powervoting-backend/task/vote.go
@@ -24,10 +24,16 @@ import (
 	"powervoting-server/model"
 	"powervoting-server/utils"
 	"strconv"
+	"sync"
+	"time"
 )
 
 // SyncVoteHandler sync vote handler
 func SyncVoteHandler() {
+	wg := sync.WaitGroup{}
+	errList := make([]error, 0)
+	mu := &sync.Mutex{}
+
 	for _, network := range config.Client.Network {
 		var proposalList []model.Proposal
 		if err := db.Engine.Model(model.Proposal{}).Where("status", 0).Where("network", network.Id).Find(&proposalList).Error; err != nil {
@@ -38,31 +44,49 @@ func SyncVoteHandler() {
 			zap.L().Error("get go-eth client error:", zap.Error(err))
 			continue
 		}
-		go func() {
-			for _, proposal := range proposalList {
-				SyncVote(ethClient, proposal.ProposalId)
-			}
-		}()
+		for _, proposal := range proposalList {
+			proposal := proposal
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				zap.L().Info("sync vote start",
+					zap.Int64("networkId", network.Id),
+					zap.Int64("proposalId", proposal.ProposalId),
+					zap.Int64("timestamp", time.Now().Unix()))
+				err := SyncVote(ethClient, proposal.ProposalId)
+				if err != nil {
+					mu.Lock()
+					errList = append(errList, err)
+					mu.Unlock()
+				}
+			}()
+		}
 	}
+	wg.Wait()
+
+	if len(errList) != 0 {
+		zap.L().Error("sync vote with err:", zap.Errors("errors", errList))
+	}
+	zap.L().Info("sync vote finished: ", zap.Int64("timestamp", time.Now().Unix()))
 }
 
 // SyncVote sync vote
-func SyncVote(ethClient model.GoEthClient, proposalId int64) {
+func SyncVote(ethClient model.GoEthClient, proposalId int64) error {
 	dictName := fmt.Sprintf("%s-%d", constant.VoteStartKey, proposalId)
 	var dict model.Dict
 	if err := db.Engine.Model(model.Dict{}).Where("name", dictName).Find(&dict).Error; err != nil {
 		zap.L().Error("Get vote start index error: ", zap.Error(err))
-		return
+		return err
 	}
 	start, err := strconv.Atoi(dict.Value)
 	if err != nil {
 		zap.L().Error("Translate string to int error: ", zap.Error(err))
-		return
+		return err
 	}
 	contractProposal, err := utils.GetProposal(ethClient, proposalId)
 	if err != nil {
 		zap.L().Error("get proposal error: ", zap.Error(err))
-		return
+		return err
 	}
 	end := int(contractProposal.VotesCount.Int64())
 	for start <= end {
@@ -79,7 +103,7 @@ func SyncVote(ethClient model.GoEthClient, proposalId int64) {
 		var count int64
 		if err = db.Engine.Model(model.Vote{}).Where("network", ethClient.Id).Where("proposal_id", proposalId).Where("address", contractVote.Voter.String()).Count(&count).Error; err != nil {
 			zap.L().Error("get vote count error: ", zap.Error(err))
-			return
+			return err
 		}
 		if count > 0 {
 			db.Engine.Model(model.Vote{}).Where("network", ethClient.Id).Where("proposal_id", proposalId).Where("address", contractVote.Voter.String()).Update("vote_info", contractVote.VoteInfo)
@@ -94,12 +118,14 @@ func SyncVote(ethClient model.GoEthClient, proposalId int64) {
 		}
 		if err = db.Engine.Model(model.Vote{}).Create(&vote).Error; err != nil {
 			zap.L().Error("create vote error: ", zap.Error(err))
-			return
+			return err
 		}
 		start++
 	}
 	if err = db.Engine.Model(model.Dict{}).Where("name", dictName).Update("value", start).Error; err != nil {
 		zap.L().Error("update vote start key error: ", zap.Error(err))
-		return
+		return err
 	}
+
+	return nil
 }
