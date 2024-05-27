d221ebdff00fc63cadde959207fc8953baeebcf6
ZeXiang <huangzexiang@storswift.com>
2024-05-27 15:36:02 +0800

    feat: Update contracts test code

--- powervoting-contracts/test/PowerVoting.test.sol
+++ powervoting-contracts/test/PowerVoting.test.sol
@@ -16,30 +16,17 @@
 pragma solidity ^0.8.19;
 
 import "../src/PowerVoting-filecoin.sol";
-import "power-oracle-contracts/Oracle.sol";
-import "forge-std/Test.sol";
 
-contract TestPowerVoting is Test {
+contract TestPowerVoting  {
     PowerVoting public powerVoting;
-    Oracle public oracle;
 
-    address public oracleAddress;
-    address public powerAddress;
     constructor() {
-        oracle = new Oracle();
-        oracleAddress = address(oracle);
-
         powerVoting = new PowerVoting();
-        powerAddress = address(powerVoting);
-
-        oracle.initialize();
-        oracle.updatePowerVotingContract(powerAddress);
-
-        powerVoting.initialize(oracleAddress);
+        powerVoting.initialize(address(this));
     }
 
-    function test_initialization() public {
-        require(powerVoting.oracleContract() == oracleAddress, "Oracle contract address mismatch");
+    function test_initialization() public view {
+        require(powerVoting.oracleContract() == address(this), "Oracle contract address mismatch");
     }
 
     function test_fip_management() public {
@@ -60,29 +47,12 @@ contract TestPowerVoting is Test {
 
         powerVoting.createProposal(proposalCid, startTime, expTime, proposalType);
 
-        (string memory cid,uint256 pType,address creator,uint248 sTime,uint248 eTime,uint256 votesCount) = powerVoting.idToProposal(1);
+        (string memory cid, uint256 pType, address creator, uint248 sTime, uint248 eTime, uint256 votesCount) = powerVoting.idToProposal(1);
         assert(keccak256(abi.encodePacked(cid)) == keccak256(abi.encodePacked(proposalCid)));
         assert(creator == address(this));
         assert(sTime == startTime);
         assert(eTime == expTime);
         assert(pType == proposalType);
         assert(votesCount == 0);
-    }
-
-    function test_voting() public {
-        powerVoting.addFIP(address(this));
-
-        string memory proposalCid = "ProposalCID";
-        uint248 startTime = uint248(block.timestamp);
-        uint248 expTime = uint248(startTime + 1);
-        uint256 proposalType = 1;
-        powerVoting.createProposal(proposalCid, startTime, expTime, proposalType);
-
-        string memory voteInfo = "I support this proposal!";
-        powerVoting.vote(1, voteInfo);
-
-        (string memory info, address voter) = powerVoting.proposalToVote(1, 1);
-        require(voter == address(this), "Voter address mismatch");
-        require(keccak256(bytes(info)) == keccak256(bytes(voteInfo)), "Vote info mismatch");
     }
 }
