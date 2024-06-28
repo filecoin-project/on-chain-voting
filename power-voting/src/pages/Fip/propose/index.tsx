4a518bd28b1683261c23137456b44662e8664161
lt <aquietuncle@gmail.com>
2024-06-28 14:27:56 +0800

    feat:update ui

--- power-voting/src/pages/Fip/propose/index.tsx
+++ power-voting/src/pages/Fip/propose/index.tsx
@@ -250,7 +250,7 @@ const FipEditorPropose = () => {
       {contextHolder}
       <div className="px-3 mb-6 md:px-0">
         <button>
-          <div className="inline-flex items-center gap-1 text-skin-text hover:text-skin-link">
+          <div className="inline-flex items-center gap-1 mb-8 text-skin-text hover:text-skin-link">
             <Link to="/home" className="flex items-center">
               <svg className="mr-1" viewBox="0 0 24 24" width="1.2em" height="1.2em">
                 <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
