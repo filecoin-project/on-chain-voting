36339de8839742d448d18bbf617a19e6b226cbcf
willpan <panpengbo@storswift.com>
2024-11-28 21:37:59 +0800

    feat: Add landing page

--- power-voting/src/pages/Landing/index.tsx
+++ power-voting/src/pages/Landing/index.tsx
@@ -2,49 +2,50 @@ import { Collapse } from "antd";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 const { Panel } = Collapse;
-
+import { useTranslation } from 'react-i18next';
 
 const Landing = () => {
+
+    const { t } = useTranslation();
 
     const DESC = [
         {
             icon: "/images/landing_ic_1.png",
-            title: "Lorem ipsum dolor sit amet1",
-            desc: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut."
+            title: t('content.section1Title'),
+            desc: t('content.section1Content'),
         },
         {
             icon: "/images/landing_ic_2.png",
-
-            title: "Lorem ipsum dolor sit amet2",
-            desc: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut."
+            title: t('content.section2Title'),
+            desc: t('content.section2Content'),
         },
         {
             icon: "/images/landing_ic_3.png",
-            title: "Lorem ipsum dolor sit amet3",
-            desc: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut."
+            title: t('content.section3Title'),
+            desc: t('content.section3Content'),
         }
 
     ]
     const QUESTIONS = [
         {
-            title: "Question 1",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi utLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.",
+            title: t('content.question1'),
+            answer: t('content.answer1'),
         },
         {
-            title: "Question 2",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi utLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.",
+            title: t('content.question2'),
+            answer: t('content.answer2'),
         },
         {
-            title: "Question 3",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi utLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.",
+            title: t('content.question3'),
+            answer: t('content.answer3'),
         },
         {
-            title: "Question 4",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi utLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.",
+            title: t('content.question4'),
+            answer: t('content.answer4'),
         },
         {
-            title: "Question 5",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi utLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.",
+            title: t('content.question5'),
+            answer: t('content.answer5'),
         }
     ]
     const navigate = useNavigate();
@@ -66,17 +67,25 @@ const Landing = () => {
             </div>
         </div>
         <div className="mt-5 text-black font-bold text-[54px] text-center">
-            Unlock the Power of Decentralized Decision-Making
+            {
+                t('content.headTitle')
+            }
         </div>
         <div className="mt-10 text-[#445063] text-[24px] px-[60px] text-center">
-            Power Voting empowers you to participate in governance and make impactful decisions within the Filecoin ecosystem.
+            {
+                t('content.headContent')
+            }
         </div>
         <div className="mt-5 w-full flex items-center justify-center">
             <div className="cursor-pointer flex items-center justify-center text-center rounded w-[128px] h-[31px] border-solid border-[1px] border-[#DFDFDF] bg-white text-[#575757]">
-                Learn More
+                {
+                    t('content.headButtonLeft')
+                }
             </div>
             <div onClick={goHome} className="cursor-pointer flex items-center justify-center ml-5 text-center rounded w-[128px] h-[31px] bg-[#0190FF] text-[#ffffff]">
-                Get Started
+                {
+                    t('content.headButtonRight')
+                }
             </div>
         </div>
 
@@ -84,13 +93,19 @@ const Landing = () => {
 
 
         <div className="mt-40 text-[#005292] text-[20px] px-[60px] text-center mb-[10px]">
-            Lorem ipsum dolor sit amet
+            {
+                t('content.topTitle')
+            }
         </div>
         <div className="mt-5 text-[#000000] text-[40px] px-[60px] text-center mb-[10px]">
-            Ut enim ad minim veniam quis nostrud
+            {
+                t('content.topHead')
+            }
         </div>
         <div className="mt-10 text-[#445063] text-[24px] px-[60px] text-center mb-[10px]">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
+            {
+                t('content.topContent')
+            }
         </div>
         <div className="flex mt-[50px]">
             {
@@ -109,7 +124,9 @@ const Landing = () => {
             }
         </div>
         <div className="mt-40 text-[#005292] text-[20px] px-[60px] text-center mb-[10px]">
-            Lorem ipsum dolor sit amet
+            {
+                t('content.questionTitle')
+            }
         </div>
         <div className="mt-10 text-[#000000] text-[40px] px-[60px] text-center mb-[10px]">
             Frequently Asked Questions
@@ -136,15 +153,21 @@ const Landing = () => {
         </Collapse>
 
         <div className="mt-40 text-[#000000] text-[40px] px-[60px] text-center mb-[10px]">
-            Ut enim ad minim veniam quis nostrud
+            {
+                t('content.bottomTitle')
+            }
         </div>
         <div className="mt-10 text-[#445063] text-[24px] px-[60px] text-center mb-[10px]">
-            Power Voting empowers you to participate in governance and make impactful decisions within the Filecoin ecosystem.
+            {
+                t('content.bottomHead')
+            }
         </div>
 
         <div className="mt-10 w-full flex items-center justify-center mb-[50px] ">
             <div onClick={goHome} className="cursor-pointer flex items-center justify-center ml-[5px] text-center rounded w-[128px] h-[31px] bg-[#0190FF] text-[#ffffff]">
-                Get Started
+                {
+                    t('content.bottomButton')
+                }
             </div>
         </div>
     </div>
