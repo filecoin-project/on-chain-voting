c405c96c36cc0e61f2a1edd300bf5bafc0005730
willpan <panpengbo@storswift.com>
2025-04-25 12:38:34 +0800

    feat: edit build.sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-IMAGE_NAME="pv-frontend-main"
+IMAGE_NAME="pv-frontend-mainnet"
 
 if [ ! -f "configuration.yaml" ]; then
     echo "Error: configuration.yaml does not exist."
