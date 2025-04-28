20a9426943f96606f4cc9f989d9cd7266c4112b8
willpan <panpengbo@storswift.com>
2025-04-28 10:33:34 +0800

    fix: ledger wallet connect error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    minify: true,
+  },
   plugins: [
     react(),
     nodePolyfills({
